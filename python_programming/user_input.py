dd1a6c6aebf8c5b2ba9468e9d090c4fdab2189f4
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-02-21 19:19:40 +0300

    Python Week1 Assignment

---
+++ python_programming/user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+location = input("Enter your location: ")
+
+print(f"Hello {name}, you are {age} years old and you live in {location}.")
